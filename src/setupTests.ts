f22f82209882aaeaec641f1430a85e6300ddb4de
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-01-28 13:50:51 +0100

    set failure threshold type to percent

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,5 +2,6 @@ import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
 expect.extend({
   toMatchImageSnapshot: configureToMatchImageSnapshot({
     failureThreshold: 0.02, // 2%
+    failureThresholdType: 'percent',
   }),
 });
